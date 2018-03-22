cedd2867efec868b1623daa33e78efed21ff33d7
Dan Schneider <danielschneidermath@gmail.com>
2018-03-22 15:05:13 -0700

    Player can add messages

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,4 +1,27 @@
 let currentMood = 2;
+let messages = [
+  {
+    owner: "",
+    message: ""
+  },
+  {
+    owner: "",
+    message: ""
+  },
+  {
+    owner: "",
+    message: ""
+  },
+  {
+    owner: "",
+    message: ""
+  },
+  {
+    owner: "",
+    message: ""
+  }
+];
+
 
 function setupMoodButtons() {
   for(let i = 0; i < 5; i++) {
@@ -17,9 +40,38 @@ function setupMoodButtons() {
   }
 }
 
-function handleChatForm(e) {
-  console.log(e);
+function addMessage(owner, message) {
+  for(let i = messages.length-1; i > 0; i--) {
+    messages[i] = messages[i-1];
+  }
+  messages[0] = {
+    owner: owner,
+    message: message
+  };
+  setMessages();
 }
+
+
+function handleChatForm(e) {
+  let playerInput = document.querySelector("#playerMessage");
+  let playerMessage = playerInput.value;
+
+  addMessage("player", playerMessage);
+  playerInput.value = "";
+}
+
+function setMessages() {
+  for(const [index, msg] of messages.entries()) {
+    let msgDiv = document.querySelector("#message" + index);
+    msgDiv.classList.remove("annie");
+    msgDiv.classList.remove("player");
+    if(msg.owner.length != 0) {
+      msgDiv.classList.add(msg.owner);
+    }
+    msgDiv.innerHTML = msg.message;
+  }
+}
+
 
 $(document).ready(function() {
   setupMoodButtons();
