f8a75d4091747b935dd22500c46ef5fe134d1147
Dan Schneider <danielschneidermath@gmail.com>
2018-03-22 13:56:16 -0700

    Made script work with document.ready() so can keep script at top

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,18 +1,18 @@
 let currentMood = 2;
 
-for(let i = 0; i < 5; i++) {
-    let moodBtn = document.querySelector('#myMood' + i);
-    moodBtn.addEventListener('click', e => {
-        if(i != currentMood) {
-          moodBtn.classList.remove('icon-unselected');
-          moodBtn.classList.add('icon-selected');
+$(document).ready(function() {
+  for(let i = 0; i < 5; i++) {
+      let moodBtn = document.querySelector('#myMood' + i);
+      moodBtn.addEventListener('click', e => {
+          if(i != currentMood) {
+            moodBtn.classList.remove('icon-unselected');
+            moodBtn.classList.add('icon-selected');
 
-          let oldMoodBtn = document.querySelector('#myMood' + currentMood);
-          oldMoodBtn.classList.remove('icon-selected');
-          oldMoodBtn.classList.add('icon-unselected');
-          currentMood = i;
-        }
-
-    });
-
-}
+            let oldMoodBtn = document.querySelector('#myMood' + currentMood);
+            oldMoodBtn.classList.remove('icon-selected');
+            oldMoodBtn.classList.add('icon-unselected');
+            currentMood = i;
+          }
+      });
+  }
+});
