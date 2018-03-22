59c388cbfba13c4a826ca101246993d7e4023558
Dan Schneider <danielschneidermath@gmail.com>
2018-03-22 15:35:15 -0700

    Got some more string stuff to work, but form is messed up

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,4 +1,5 @@
 let currentMood = 2;
+let playerName = "";
 let messages = [
   {
     owner: "",
@@ -53,7 +54,7 @@ function addMessage(owner, message) {
 
 function annieResponse(playerMessage) {
   playerMessage = playerMessage.toLowerCase();
-  annieMessage = "I'm bored. You're boring. I'm gonna wait for someone else to talk to";
+  annieMessage = "";
 
   if(playerMessage.includes("walk")) {
     annieMessage = "DID YOU SAY WALK?!?! Let's go on a walk! RIGHT NOW!";
@@ -62,20 +63,39 @@ function annieResponse(playerMessage) {
     annieMessage = "DID YOU SAY PARK?!?! Let's go to the park! RIGHT NOW!";
   }
   if(playerMessage.includes("food")) {
-    annieMessage = "Food? Where? I want some. Give me food.";
+    annieMessage = `Food? Where? I want some. Give me food ${(name.length != 0 ? name : "")}!`;
   }
   if(playerMessage.includes("?")) {
     annieMessage = "I know you asked me a question but I'm just going to ignore it and stare at you because I'm a dumb dog";
   }
   if(playerMessage.includes("i like")) {
       let temp = playerMessage.split("i like ")[1].split(" ")[0];
-      console.log(temp);
       annieMessage = `WHOA! I like ${temp} too! Since we have something in common, PET ME!`;
   }
   if(playerMessage.includes("i hate")) {
       let temp = playerMessage.split("i hate ")[1].split(" ")[0];
-      console.log(temp);
       annieMessage = `Yah I hate ${temp} too! Since we have something in common, PET ME!`;
+  }
+  if(playerMessage.includes("my name is")) {
+      let temp = playerMessage.split("my name is ")[1].split(" ")[0];
+      name = temp;
+      annieMessage = `Hi ${name}! Let's talk. Talk talk talk. I'm a dog.`;
+  }
+
+
+  if(annieMessage.length == 0) {
+    let rand = Math.floor(Math.random() * 5);
+    if(rand == 0) {
+        annieMessage = "I'm bored. You're boring. I'm gonna wait for someone else to talk to";
+    } else if (rand == 1) {
+        annieMessage = "WAIT.... Did you hear that?! I heard something...";
+    } else if (rand == 2) {
+        annieMessage = "I IGNORED WHAT YOU SAID BECAUSE I'M EXCITED YOU'RE HERE";
+    } else if (rand == 3) {
+        annieMessage = "I'M A SPACE DOG!";
+    } else if (rand == 4) {
+        annieMessage = "That's a very informed and intellectual thought that I don't understand because I'm just a dumb dog";
+    }
   }
 
   addMessage("annie", annieMessage);
